876517c8449a9c5aa1f35a09ec3685518c674465
Jacek Zygiel <jacekzygiel@gmail.com>
2020-01-08 21:56:15 +0100

    Adds cheers endpoint to api

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/hello')
 def hello_jlabs():
     return '<h1 style="color:red;">Hello, Jlabs!</h1>'
+
+
+@app.route('/cheers')
+def hello_jlabs():
+    return '<h1 style="color:red;">Cheers, Jlabs!!</h1>'
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
