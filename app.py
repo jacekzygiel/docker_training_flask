3295e08d7a833dd53f3769575923192ca5ff32ac
jacekzygiel <jacekzygiel@users.noreply.github.com>
2020-09-09 12:01:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/hello')
-def hello_jlabs():
+def hello_hsbc():
     return '<h1 style="color:red;">Hello, HSBC!</h1>'
 
 if __name__ == '__main__':
