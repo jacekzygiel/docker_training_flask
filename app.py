dad3d583e12b9ead16b373ff2b25a66b1041ff08
jacekzygiel <jacekzygiel@users.noreply.github.com>
2020-09-09 12:00:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def hello_jlabs():
-    return '<h1 style="color:red;">Hello, Jlabs!</h1>'
+    return '<h1 style="color:red;">Hello, HSBC!</h1>'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
